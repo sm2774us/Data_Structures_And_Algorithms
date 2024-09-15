da7901e98bcea16dc587ff8cac9be9305d9533d4
Shaikat Majumdar <sm2774us@gmal.com>
2024-09-14 22:00:47 -0400

    initial project import

---
+++ sm2774us/version/__init__.py
@@ -0,0 +1,8 @@
+"""
+initialization file for sm2774us
+"""
+
+from sm2774us.version.sm2774us_version import __version__  # noqa: F401
+
+__app_id__ = "sm2774us/Data_Structures_And_Algorithms"
+__app_name__ = "Data_Structures_And_Algorithms"
